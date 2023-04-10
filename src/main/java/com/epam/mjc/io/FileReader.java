1c48816e3e4c4c8f1d9170e33e4f481fe1935ab0
ZakharSytoi <zakharsytoi@gmail.com>
2023-04-10 19:38:14 +0200

    Task completed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        java.io.FileReader inputStream;
+        StringBuilder tmpText = new StringBuilder();
+        Profile result = new Profile();
+        try {
+            inputStream = new java.io.FileReader(file);
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                tmpText.append((char)c);
+            }
+            String textFinal = String.valueOf(tmpText);
+            result.setName(textFinal.split("\n")[0].split(": ")[1]);
+            result.setAge(Integer.parseInt(textFinal.split("\n")[1].split(": ")[1]));
+            result.setEmail(textFinal.split("\n")[2].split(": ")[1]);
+            result.setPhone(Long.parseLong(textFinal.split("\n")[3].split(": ")[1]));
+
+            inputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 }
